3345db7e1fa89d014db7a62913a8b13aea7ce1df
tatiwebdev <58571291+tatiwebdev@users.noreply.github.com>
2020-03-19 01:15:10 +0300

    Add files via upload

--- mailer/smart.php
+++ mailer/smart.php
@@ -14,13 +14,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'canditt@mail.ru';                 // Наш логин
-$mail->Password = 'tank06061989';                           // Наш пароль от ящика
+$mail->Username = 'digitaltelsis@mail.ru';                 // Наш логин
+$mail->Password = 'digital2020';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('canditt@mail.ru', 'Perfomance');   // От кого письмо 
-$mail->addAddress('canditt@list.ru');     // Add a recipient
+$mail->setFrom('digitaltelsis@mail.ru', 'Digital');   // От кого письмо 
+$mail->addAddress('digital@tel-sis.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
